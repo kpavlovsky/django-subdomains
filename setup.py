9fa976f63f4f61ec029e4a0d46ca830e759dd95b
Konstantin Pavlovsky <cto@ecompilot.ru>
2018-10-28 07:47:12 +0300

    updated django versions

--- setup.py
+++ setup.py
@@ -9,28 +9,28 @@ def get_version():
     from subdomains import __version__
     return '.'.join(map(str, __version__))
 
+
 try:
     version = get_version()
 except ImportError:
     sys.path.append(os.path.join(os.path.dirname(__file__, 'subdomains')))
     version = get_version()
 
-
 install_requires = ['django']
 tests_require = install_requires + ['mock']
 
 setup(name='django-subdomains',
-    version=version,
-    url='http://github.com/tkaemming/django-subdomains/',
-    author='ted kaemming',
-    author_email='ted@kaemming.com',
-    description="Subdomain tools for the Django framework, including "
-        "subdomain-based URL routing.",
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=install_requires,
-    tests_require=tests_require,
-    test_suite='subdomains.tests.run',
-    zip_safe=False,
-    license='MIT License',
-)
+      version=version,
+      url='http://github.com/tkaemming/django-subdomains/',
+      author='ted kaemming',
+      author_email='ted@kaemming.com',
+      description="Subdomain tools for the Django framework, including "
+                  "subdomain-based URL routing.",
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=install_requires,
+      tests_require=tests_require,
+      test_suite='subdomains.tests.run',
+      zip_safe=False,
+      license='MIT License',
+      )
